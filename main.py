7ca13a1f77bf3cd565895e95ac681b0fff98f256
Sa2lite <145333049+Sa2lite@users.noreply.github.com>
2024-07-06 03:53:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,15 @@ class VacancyForm(StatesGroup):
     vacancies_per_page = State()
     current_vacancy_index = State()
 
+class ResumeForm(StatesGroup):
+    job_title = State()
+    work_experience = State()
+    age = State()
+    resumes_per_page = State()
+    current_resume_index = State()
+
 def create_database():
-    conn = sqlite3.connect('vacancies.sql')
+    conn = sqlite3.connect('vacancies_and_resumes.sql')
     cursor = conn.cursor()
 
     cursor.execute('''
@@ -41,11 +48,24 @@ def create_database():
         )
     ''')
 
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS resumes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            job_title TEXT,
+            gender TEXT,
+            age TEXT,
+            birthday TEXT,
+            work_experience TEXT,
+            last_job_duration TEXT,
+            resume_url TEXT
+        )
+    ''')
+
     conn.commit()
     conn.close()
 
 def insert_vacancy(vacancy_data):
-    conn = sqlite3.connect('vacancies.sql')
+    conn = sqlite3.connect('vacancies_and_resumes.sql')
     cursor = conn.cursor()
 
     cursor.execute('''
@@ -56,8 +76,22 @@ def insert_vacancy(vacancy_data):
     conn.commit()
     conn.close()
 
+def insert_resume(resume_data):
+    conn = sqlite3.connect('vacancies_and_resumes.sql')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        INSERT INTO resumes (job_title, gender, age, birthday, work_experience, last_job_duration, resume_url)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+    ''', resume_data)
+
+    conn.commit()
+    conn.close()
+
+
+
 def get_all_vacancies():
-    conn = sqlite3.connect('vacancies.sql')
+    conn = sqlite3.connect('vacancies_and_resumes.sql')
     cursor = conn.cursor()
 
     cursor.execute('SELECT * FROM vacancies')
@@ -67,7 +101,7 @@ def get_all_vacancies():
     return vacancies
 
 def get_vacancies_by_params(params):
-    conn = sqlite3.connect('vacancies.sql')
+    conn = sqlite3.connect('vacancies_and_resumes.sql')
     cursor = conn.cursor()
 
     query = "SELECT * FROM vacancies WHERE 1=1"
@@ -83,11 +117,39 @@ def get_vacancies_by_params(params):
     conn.close()
     return vacancies
 
+def get_all_resumes():
+    conn = sqlite3.connect('vacancies_and_resumes.sql')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM resumes')
+    resumes = cursor.fetchall()
+
+    conn.close()
+    return resumes
+
+def get_resumes_by_params(params):
+    conn = sqlite3.connect('vacancies_and_resumes.sql')
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM resumes WHERE 1=1"
+    query_params = []
+    for key, value in params.items():
+        if key != 'resumes_per_page' and key != 'current_resume_index' and value:
+            query += f" AND {key} LIKE ?"
+            query_params.append(f"%{value}%")
+
+    cursor.execute(query, query_params)
+    resumes = cursor.fetchall()
+
+    conn.close()
+    return resumes
+
 def clear_database():
-    conn = sqlite3.connect('vacancies.sql')
+    conn = sqlite3.connect('vacancies_and_resumes.sql')
     cursor = conn.cursor()
 
     cursor.execute('DELETE FROM vacancies')
+    cursor.execute('DELETE FROM resumes')
 
     conn.commit()
     conn.close()
@@ -104,7 +166,7 @@ async def start_button(message: types.Message):
         [InlineKeyboardButton(text="Работодатель", callback_data="role_employer")]
     ])
     await message.answer('Выберите вашу роль:', reply_markup=keyboard)
-
+#соискатель___________________________________________________________________________________________________________
 @dp.callback_query(lambda c: c.data == 'role_applicant')
 async def process_callback_applicant(callback_query: types.CallbackQuery, state: FSMContext):
     await bot.answer_callback_query(callback_query.id)
@@ -120,6 +182,20 @@ async def process_callback_applicant(callback_query: types.CallbackQuery, state:
         [InlineKeyboardButton(text="Показать вакансии", callback_data="show_vacancies")]
     ])
     await bot.send_message(callback_query.from_user.id, 'Выберите параметр для отображения:', reply_markup=keyboard)
+
+@dp.callback_query(lambda c: c.data == 'role_employer')
+async def process_callback_employer(callback_query: types.CallbackQuery, state: FSMContext):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Работодатель выбран.')
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Профессия", callback_data="resume_job_title")],
+        [InlineKeyboardButton(text="Опыт работы", callback_data="resume_work_experience")],
+        [InlineKeyboardButton(text="Возраст", callback_data="resume_age")],
+        [InlineKeyboardButton(text="Количество резюме за раз", callback_data="resume_count_per_page")],
+        [InlineKeyboardButton(text="Показать резюме", callback_data="show_resumes")]
+    ])
+    await bot.send_message(callback_query.from_user.id, 'Выберите параметр для отображения:', reply_markup=keyboard)
+    
 
 @dp.callback_query(lambda c: c.data == 'vacancy_title')
 async def process_callback_vacancy_title(callback_query: types.CallbackQuery, state: FSMContext):
@@ -362,6 +438,258 @@ def parse_multiple_pages(base_url, start_page, end_page):
             break
         url = f"{base_url}&page={page}"
         parse_vacancies(url)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+#Работодатель      
+
+# Обработчики для кнопок "Работодатель"
+@dp.callback_query(lambda c: c.data == 'resume_job_title')
+async def process_callback_resume_job_title(callback_query: types.CallbackQuery, state: FSMContext):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Укажите профессию:')
+    await state.set_state(ResumeForm.job_title)
+
+@dp.message(ResumeForm.job_title)
+async def process_resume_job_title(message: types.Message, state: FSMContext):
+    await state.update_data(job_title=message.text)
+    await state.set_state(None)
+    await update_resume_keyboard(message.from_user.id, state)
+
+@dp.callback_query(lambda c: c.data == 'resume_work_experience')
+async def process_callback_resume_work_experience(callback_query: types.CallbackQuery, state: FSMContext):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Укажите опыт работы:')
+    await state.set_state(ResumeForm.work_experience)
+
+@dp.message(ResumeForm.work_experience)
+async def process_resume_work_experience(message: types.Message, state: FSMContext):
+    await state.update_data(work_experience=message.text)
+    await state.set_state(None)
+    await update_resume_keyboard(message.from_user.id, state)
+
+@dp.callback_query(lambda c: c.data == 'resume_age')
+async def process_callback_resume_age(callback_query: types.CallbackQuery, state: FSMContext):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Укажите возраст:')
+    await state.set_state(ResumeForm.age)
+
+@dp.message(ResumeForm.age)
+async def process_resume_age(message: types.Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await state.set_state(None)
+    await update_resume_keyboard(message.from_user.id, state)
+
+@dp.callback_query(lambda c: c.data == 'resume_count_per_page')
+async def process_callback_resume_count_per_page(callback_query: types.CallbackQuery, state: FSMContext):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Укажите количество резюме, которые будут показываться за один поиск:')
+    await state.set_state(ResumeForm.resumes_per_page)
+
+@dp.message(ResumeForm.resumes_per_page)
+async def process_resume_count_per_page(message: types.Message, state: FSMContext):
+    try:
+        resumes_per_page = int(message.text)
+        await state.update_data(resumes_per_page=resumes_per_page)
+        await state.set_state(None)
+        await update_resume_keyboard(message.from_user.id, state)
+    except ValueError:
+        await bot.send_message(message.from_user.id, 'Пожалуйста, введите число.')
+
+async def update_resume_keyboard(user_id, state: FSMContext):
+    data = await state.get_data()
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text=f"Профессия: {data.get('job_title', 'Не указано')}", callback_data="resume_job_title")],
+        [InlineKeyboardButton(text=f"Опыт работы: {data.get('work_experience', 'Не указано')}", callback_data="resume_work_experience")],
+        [InlineKeyboardButton(text=f"Возраст: {data.get('age', 'Не указано')}", callback_data="resume_age")],
+        [InlineKeyboardButton(text=f"Количество резюме за раз: {data.get('resumes_per_page', 'Не указано')}", callback_data="resume_count_per_page")],
+        [InlineKeyboardButton(text="Показать резюме", callback_data="show_resumes")]
+    ])
+    await bot.send_message(user_id, 'Выберите параметр для отображения:', reply_markup=keyboard)
+
+@dp.callback_query(lambda c: c.data == 'show_resumes')
+async def process_callback_show_resumes(callback_query: types.CallbackQuery, state: FSMContext):
+    await bot.answer_callback_query(callback_query.id)
+    data = await state.get_data()
+    resumes = get_resumes_by_params(data)
+    resumes_per_page = data.get('resumes_per_page', 5)
+    current_index = data.get('current_resume_index', 0)
+
+    if not resumes:
+        keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Начать парсинг", callback_data="start_parsing_resumes")]])
+        await bot.send_message(callback_query.from_user.id, 'Резюме не найдены. Начать парсинг?', reply_markup=keyboard)
+        return
+
+    end_index = current_index + resumes_per_page
+    resumes_to_show = resumes[current_index:end_index]
+
+    if resumes_to_show:
+        for resume in resumes_to_show:
+            resume_text = f"Профессия: {resume[1]}\n"
+            resume_text += f"Пол: {resume[2]}\n"
+            resume_text += f"Возраст: {resume[3]}\n"
+            resume_text += f"День рождения: {resume[4]}\n"
+            resume_text += f"Опыт работы: {resume[5]}\n"
+            resume_text += f"Длительность последней работы: {resume[6]}\n"
+            resume_text += f"Ссылка на резюме: {resume[7]}\n\n"
+            await bot.send_message(callback_query.from_user.id, resume_text)
+
+        if end_index < len(resumes):
+            await state.update_data(current_resume_index=end_index)
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Показать еще", callback_data="show_resumes")]])
+            await bot.send_message(callback_query.from_user.id, 'Показать еще резюме?', reply_markup=keyboard)
+        else:
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Начать парсинг", callback_data="start_parsing_resumes")]])
+            await bot.send_message(callback_query.from_user.id, 'Больше резюме нет. Начать парсинг?', reply_markup=keyboard)
+    else:
+        keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Начать парсинг", callback_data="start_parsing_resumes")]])
+        await bot.send_message(callback_query.from_user.id, 'Больше резюме нет. Начать парсинг?', reply_markup=keyboard)
+
+parsing_active = False
+
+@dp.callback_query(lambda c: c.data == 'start_parsing_resumes')
+async def start_parsing_resumes(callback_query: types.CallbackQuery, state: FSMContext):
+    global parsing_active
+    await bot.answer_callback_query(callback_query.id)
+    if not parsing_active:
+        parsing_active = True
+        await bot.send_message(callback_query.from_user.id, 'Начинаем парсинг резюме...')
+        keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Остановить парсинг", callback_data="stop_parsing_resumes")]])
+        await bot.send_message(callback_query.from_user.id, 'Нажмите кнопку ниже, чтобы остановить парсинг.', reply_markup=keyboard)
+
+        data = await state.get_data()
+        job_title = data.get('job_title', 'python')
+        conn = sqlite3.connect('vacancies_and_resumes.sql')
+
+        await get_resumes(job_title, 0, 2, conn)
+
+        await bot.send_message(callback_query.from_user.id, 'Парсинг резюме завершен.')
+    else:
+        await bot.send_message(callback_query.from_user.id, 'Парсинг резюме уже запущен.')
+    
+
+@dp.callback_query(lambda c: c.data == 'stop_parsing_resumes')
+async def stop_parsing_resumes(callback_query: types.CallbackQuery):
+    global parsing_active
+    await bot.answer_callback_query(callback_query.id)
+    if parsing_active:
+        parsing_active = False
+        await bot.send_message(callback_query.from_user.id, 'Парсинг резюме остановлен.')
+    else:
+        await bot.send_message(callback_query.from_user.id, 'Парсинг резюме не запущен.')
+
+async def get_resumes(query, start_page, end_page, conn):
+    base_url = "https://hh.ru/search/resume"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
+    
+    for page in range(start_page, end_page + 1):
+        if not parsing_active:
+            break
+        params = {
+            "text": query,
+            "pos": "full_text",
+            "logic": "normal",
+            "exp_period": "all_time",
+            "ored_clusters": "true",
+            "order_by": "relevance",
+            "search_period": "0",
+            "page": page
+        }
+        
+        response = requests.get(base_url, headers=headers, params=params)
+        
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            resume_links = soup.find_all('a', {'data-qa': 'serp-item__title'})
+            
+            for link in resume_links:
+                if not parsing_active:
+                    break
+                resume_url = "https://hh.ru" + link['href']
+                print(f"Processing resume: {resume_url}")
+                resume_data = get_resume_details(resume_url, headers)
+                if resume_data:
+                    resume_data['resume_url'] = resume_url
+                    save_resume_data(resume_data, conn)
+        else:
+            print(f"Failed to retrieve page {page}. Status code: {response.status_code}")
+
+def get_resume_details(resume_url, headers):
+    response = requests.get(resume_url, headers=headers)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        job_title_tag = soup.find('h2', {'data-qa': 'bloko-header-2'})
+        job_title = job_title_tag.get_text(strip=True) if job_title_tag else "Job Title not found"
+        
+        personal_info_tag = soup.find('p')
+        gender = "Gender not found"
+        age = "Age not found"
+        birthday = "Birthday not found"
+        if personal_info_tag:
+            gender_tag = personal_info_tag.find('span', {'data-qa': 'resume-personal-gender'})
+            age_tag = personal_info_tag.find('span', {'data-qa': 'resume-personal-age'})
+            birthday_tag = personal_info_tag.find('span', {'data-qa': 'resume-personal-birthday'})
+            
+            gender = gender_tag.get_text(strip=True) if gender_tag else "Gender not found"
+            age = age_tag.get_text(strip=True) if age_tag else "Age not found"
+            birthday = birthday_tag.get_text(strip=True) if birthday_tag else "Birthday not found"
+        
+        experience_tag = soup.find('span', {'class': 'resume-block__title-text resume-block__title-text_sub'})
+        work_experience = experience_tag.get_text(strip=True) if experience_tag else "Work Experience not found"
+        
+        last_job_tag = soup.find('div', {'class': 'bloko-column bloko-column_xs-4 bloko-column_s-2 bloko-column_m-2 bloko-column_l-2'})
+        last_job_duration = last_job_tag.get_text(strip=True) if last_job_tag else "Last Job Duration not found"
+        
+        return {
+            "job_title": job_title,
+            "gender": gender,
+            "age": age,
+            "birthday": birthday,
+            "work_experience": work_experience,
+            "last_job_duration": last_job_duration
+        }
+    else:
+        print(f"Failed to retrieve resume details. Status code: {response.status_code}")
+        return None
+
+def save_resume_data(resume_data, conn):
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO resumes (job_title, gender, age, birthday, work_experience, last_job_duration, resume_url)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+    ''', (
+        resume_data['job_title'],
+        resume_data['gender'],
+        resume_data['age'],
+        resume_data['birthday'],
+        resume_data['work_experience'],
+        resume_data['last_job_duration'],
+        resume_data['resume_url']
+    ))
+    conn.commit()
 
 async def main():
     await dp.start_polling(bot)
